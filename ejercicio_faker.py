18c3c71ca6eef71bfc4233a22ebc79b035db1c39
jota <juan.erices04@inacapmail.cl>
2025-10-12 16:02:05 -0300

    first commit

---
+++ ejercicio_faker.py
@@ -0,0 +1,15 @@
+"""
+    * Generar un script de python con Faker para poblar una base de datos con un millon de registros.
+    
+    j0t4InC0de
+"""
+
+# ? HACER CONEXCION CON LA BASE DE DATOS
+
+#? CONFIGURACION DE LA GENERACIÓN DE DATOS
+
+#? FUNCIONES AUXILIARES
+
+#? FUNCIONES DE GENERACIÓN E INSERCIÓN DE DATOS
+
+#* SCRIPT PRINCIPAL
