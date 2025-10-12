e1b864287e9ec29ab8918411b22b5d92e6819213
jota <juan.erices04@inacapmail.cl>
2025-10-12 16:17:50 -0300

    Hacer conexion con la base de datos mediante Xampp

--- ejercicio_faker.py
+++ ejercicio_faker.py
@@ -1,10 +1,25 @@
 """
     * Generar un script de python con Faker para poblar una base de datos con un millon de registros.
     
+    pip install Faker
+    pip install mysql-connector-python
+    
     j0t4InC0de
 """
+import mysql.connector
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+import time
 
-# ? HACER CONEXCION CON LA BASE DE DATOS
+#? HACER CONEXCION CON LA BASE DE DATOS
+db_config = {
+    'host': 'localhost',
+    'user': 'admin',
+    'password': 'admin',
+    'database': 'bd_instituto'
+}
+print("Conectando a la base de datos...\n", db_config)
 
 #? CONFIGURACION DE LA GENERACIÓN DE DATOS
 
